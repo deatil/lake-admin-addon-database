2846ea080ed5f819db79d26b51ebad976473f489
deatil <2217957370@qq.com>
2020-07-27 13:09:09 +0800

    常规更新

--- src/addons/database/admin/controller/Database.php
+++ src/addons/database/admin/controller/Database.php
@@ -34,7 +34,7 @@ class Database extends AdminBase
             $this->error('请先进行相关配置！');
         }
         
-        $this->databaseConfig = array(
+        $this->databaseConfig = [
             //数据库备份根路径（路径必须以 / 结尾）
             'path' => root_path() . $config['path'],
             //数据库备份卷大小 （该值用于限制压缩后的分卷最大长度。单位：B；建议设置20M）
@@ -43,10 +43,15 @@ class Database extends AdminBase
             'compress' => (int) $config['compress'],
             //数据库备份文件压缩级别 （数据库备份文件的压缩级别，该配置在开启压缩时生效） 1普通 4一般 9最高
             'level' => (int) $config['level'],
-        );
+        ];
     }
 
-    //数据库备份
+    /**
+     * 数据库
+     * 
+     * @create 2019-7-19
+     * @author deatil
+     */
     public function index()
     {
         if ($this->request->isAjax()) {
@@ -58,11 +63,15 @@ class Database extends AdminBase
                 "data" => $list,
             ]);
         }
+        
         return $this->fetch();
     }
 
     /**
      * 备份数据库
+     * 
+     * @create 2019-7-19
+     * @author deatil
      */
     public function export()
     {
@@ -78,32 +87,49 @@ class Database extends AdminBase
             if (!is_dir($config['path'])) {
                 mkdir($config['path'], 0755, true);
             }
+            
             // 检查是否有正在执行的任务
-            $lock = "{$config['path']}backup.lock";
+            $lock = $config['path'] . "backup.lock";
             if (is_file($lock)) {
+                // 超过30分钟清除锁定文件，防止后期不能再备份
+                $backupMtime = filemtime($lock);
+                if (($backupMtime + 1800) < time()) {
+                    unlink($lock);
+                }
                 return $this->error('检测到有一个备份任务正在执行，请稍后再试！');
-            } else {
-                // 创建锁文件
-                file_put_contents($lock, time());
             }
+            
+            // 创建锁文件
+            file_put_contents($lock, time());
+                
             // 检查备份目录是否可写
             if (!is_writeable($config['path'])) {
                 return $this->error('备份目录不存在或不可写，请检查后重试！');
             }
+            
             session('backup_config', $config);
+            
             // 生成备份文件信息
-            $file = array(
+            $file = [
                 'name' => date('Ymd-His', time()),
                 'part' => 1,
-            );
+            ];
+            
             session('backup_file', $file);
             // 缓存要备份的表
             session('backup_tables', $tables);
+            
             // 创建备份文件
             $Database = new DatabaseService($file, $config);
             if (false !== $Database->create()) {
-                $tab = array('id' => 0, 'start' => 0);
-                return $this->success('初始化成功！', '', array('tables' => $tables, 'tab' => $tab));
+                $tab = [
+                    'id' => 0, 
+                    'start' => 0,
+                ];
+                return $this->success('初始化成功！', '', [
+                    'tables' => $tables, 
+                    'tab' => $tab,
+                ]);
             } else {
                 return $this->error('初始化失败，备份文件创建失败！');
             }
@@ -119,28 +145,30 @@ class Database extends AdminBase
             } elseif (0 === $start) {
                 // 下一表
                 if (isset($tables[++$id])) {
-                    $tab = array('id' => $id, 'start' => 0);
-                    return $this->success('备份完成！', '', array(
+                    $tab = [
+                        'id' => $id, 
+                        'start' => 0,
+                    ];
+                    return $this->success('备份完成！', '', [
                         'tab' => $tab, 
-                        'table' => $tables[$id-1], 
-                        'nexttable' => $tables[$id]
-                    ));
+                    ]);
                 } else {
                     // 备份完成，清空缓存
                     unlink(session('backup_config.path') . 'backup.lock');
                     session('backup_tables', null);
                     session('backup_file', null);
                     session('backup_config', null);
-                    return $this->success('备份完成！', '', array('table' => $tables[$id-1]));
+                    return $this->success('备份完成！');
                 }
             } else {
-                $tab = array('id' => $id, 'start' => $start[0]);
+                $tab = [
+                    'id' => $id, 
+                    'start' => $start[0],
+                ];
                 $rate = floor(100 * ($start[0] / $start[1]));
-                return $this->success("正在备份...({$rate}%)", '', array(
+                return $this->success("正在备份...({$rate}%)", '', [
                     'tab' => $tab, 
-                    'table' => $tables[$id],
-                    'nexttable' => $tables[$id+1]
-                ));
+                ]);
             }
 
         } else {
@@ -149,17 +177,22 @@ class Database extends AdminBase
 
     }
 
-    // 备份恢复
+    /**
+     * 备份恢复
+     * 
+     * @create 2019-7-19
+     * @author deatil
+     */
     public function restore()
     {
         if ($this->request->isAjax()) {
-            //列出备份文件列表
+            // 列出备份文件列表
             $path = $this->databaseConfig['path'];
             $glob = glob($path . '*.gz', GLOB_BRACE);
-            $list = array();
+            $list = [];
             foreach ($glob as $key => $file) {
                 $fileInfo = pathinfo($file);
-                //文件名
+                // 文件名
                 $name = $fileInfo['basename'];
                 if (preg_match('/^\d{8,8}-\d{6,6}-\d+\.sql(?:\.gz)?$/', $name)) {
                     $name = sscanf($name, '%4s%2s%2s-%2s%2s%2s-%d');
@@ -185,7 +218,10 @@ class Database extends AdminBase
                     $list[$key] = $info;
                 }
             }
-            $result = array("code" => 0, "data" => $list);
+            $result = [
+                "code" => 0, 
+                "data" => $list,
+            ];
             return json($result);
         } else {
             return $this->fetch('import');
@@ -193,7 +229,12 @@ class Database extends AdminBase
 
     }
 
-    //下载
+    /**
+     * 下载
+     * 
+     * @create 2019-7-19
+     * @author deatil
+     */
     public function download()
     {
         $time = $id = $this->request->param('time/d');
@@ -217,6 +258,9 @@ class Database extends AdminBase
 
     /**
      * 还原数据库
+     * 
+     * @create 2019-7-19
+     * @author deatil
      */
     public function import()
     {
@@ -230,39 +274,48 @@ class Database extends AdminBase
             $name = date('Ymd-His', $time) . '-*.sql*';
             $path = $this->databaseConfig['path'] . $name;
             $files = glob($path);
-            $list = array();
+            $list = [];
             foreach ($files as $name) {
                 $basename = basename($name);
                 $match = sscanf($basename, '%4s%2s%2s-%2s%2s%2s-%d');
                 $gz = preg_match('/^\d{8,8}-\d{6,6}-\d+\.sql.gz$/', $basename);
-                $list[$match[6]] = array($match[6], $name, $gz);
+                $list[$match[6]] = [$match[6], $name, $gz];
             }
             ksort($list);
-            //检测文件正确性
+            // 检测文件正确性
             $last = end($list);
             if (count($list) === $last[0]) {
                 session('backup_list', $list); //缓存备份列表
-                return $this->success('初始化完成！', '', array('part' => 1, 'start' => 0));
+                return $this->success('初始化完成！', '', [
+                    'part' => 1, 
+                    'start' => 0,
+                ]);
             } else {
                 return $this->error('备份文件可能已经损坏，请检查！');
             }
         } elseif (is_numeric($part) && is_numeric($start)) {
             $list = session('backup_list');
-            $db = new DatabaseService($list[$part], array('path' => realpath(config('app.data_backup_path')) . DIRECTORY_SEPARATOR, 'compress' => $list[$part][2]));
+            $db = new DatabaseService($list[$part], [
+                'path' => realpath(config('app.data_backup_path')) . DIRECTORY_SEPARATOR, 
+                'compress' => $list[$part][2],
+            ]);
             $start = $db->import($start);
             if (false === $start) {
                 return $this->error('还原数据出错！');
             } elseif (0 === $start) {
                 //下一卷
                 if (isset($list[++$part])) {
-                    $data = array('part' => $part, 'start' => 0);
+                    $data = ['part' => $part, 'start' => 0];
                     return $this->success("正在还原...#{$part}", '', $data);
                 } else {
                     session('backup_list', null);
                     return $this->success('还原完成！');
                 }
             } else {
-                $data = array('part' => $part, 'start' => $start[0]);
+                $data = [
+                    'part' => $part, 
+                    'start' => $start[0],
+                ];
                 if ($start[1]) {
                     $rate = floor(100 * ($start[0] / $start[1]));
                     return $this->success("正在还原...#{$part} ({$rate}%)", '', $data);
@@ -279,6 +332,9 @@ class Database extends AdminBase
     /**
      * 删除备份文件
      * @param  Integer $time 备份时间
+     * 
+     * @create 2019-7-19
+     * @author deatil
      */
     public function del()
     {
@@ -300,6 +356,9 @@ class Database extends AdminBase
     /**
      * 优化表
      * @param  String $tables 表名
+     * 
+     * @create 2019-7-19
+     * @author deatil
      */
     public function optimize()
     {
@@ -323,6 +382,9 @@ class Database extends AdminBase
     /**
      * 修复表
      * @param  String $tables 表名
+     * 
+     * @create 2019-7-19
+     * @author deatil
      */
     public function repair()
     {
